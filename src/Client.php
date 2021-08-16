bd626e5fcc0d5d5c6a687fd57af4a72c98db01bf
ryan <chenzhenlu@yaoyungroup.com>
2021-08-16 14:32:28 +0800

    修改包内容

--- src/Client.php
+++ src/Client.php
@@ -4,18 +4,20 @@ declare(strict_types=1);
 namespace TopdrinkHyperf\Apollo;
 
 
-use Hyperf\ConfigApollo\ClientInterface;
-use Psr\Container\ContainerInterface;
+use Symfony\Component\Yaml\Yaml;
 
 class Client
 {
-    public function __construct(ContainerInterface $container)
-    {
-        
-    }
-
-    public function parallelPull($namespaces){
-        echo 123;
-        $config = $this->client->parallelPull($namespaces);
+    /**
+     * Notes:生成yml文件
+     * User: ryan
+     * DateTime: 2021/8/16 14:23
+     * @param array $array
+     */
+    public function createYmlFile(array $array){
+        foreach ($array as $key=>$value){
+            $yaml = Yaml::dump($value);
+            file_put_contents('./'.$key.'.yaml',$yaml);
+        }
     }
 }
